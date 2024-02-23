737da4f4aea429f77949c73ae96bf3d284e1b27e
jk_hari <104263832+JKHari@users.noreply.github.com>
2024-02-23 20:02:02 +0530

    create a app

---
+++ main.js
@@ -0,0 +1,41 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+const url = require('url');
+let mainWindow;
+
+require('electron-reload')(__dirname, {
+    electron: require(`${__dirname}/node_modules/electron`),
+    awaitWriteFinish: true,
+});
+
+app.on('window-all-closed', function () {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+})
+
+app.on('ready', () => {
+    mainWindow = new BrowserWindow({
+        width: 355,
+        height: 580,
+        icon: path.join(__dirname, 'icons', 'backspace.svg'), // Change 'cal.svg' to 'cal.png' or 'cal.ico'
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false,
+            worldSafeExecuteJavaScript: true,
+            allowRunningInsecureContent: true,
+        }
+    })
+
+    mainWindow.loadURL(url.format({
+        pathname: path.join(__dirname, 'index.html'),
+        protocol: 'file',
+        slashes: true
+    }));
+    mainWindow.setMenu(null);
+
+    mainWindow.on('closed', () => {
+        mainWindow = null;
+    })
+
+})
